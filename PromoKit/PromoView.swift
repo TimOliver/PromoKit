7646208038dd3f77cadb1c74a61520dcb42cee59
Tim Oliver <git@tim.dev>
2024-03-17 17:04:19 +0900

    Added common properties to promo view

--- PromoKit/PromoView.swift
+++ PromoKit/PromoView.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-/// A promo view displays promotional or advertising content from a variety of sources,
+/// A UI component for displaying promotional or advertising content from a variety of sources,
 /// determined and updated dynamically at runtime.
 /// It can be used to show a regular set of content, such as ads, but allow for higher priority
 /// content, such as app news announcements to automatically override and display instead.
@@ -15,13 +15,52 @@ import UIKit
 @objc(PMKPromoView)
 public class PromoView: UIView {
 
+    // MARK: - Public Properties -
+
+    /// The content view from the currently active promo provider
+    public var contentView: UIView?
+
+    /// The corner radius of the promo view
+    public var cornerRadius: CGFloat {
+        set { backgroundView.layer.cornerRadius = newValue }
+        get { backgroundView.layer.cornerRadius }
+    }
+
+    /// Whether a close button is shown on the trailing side of the ad view (Default is false)
+    public var showCloseButton: Bool = false
+
+    /// The background view displayed behind the content view
+    private(set) public var backgroundView: UIView
+
+    /// The promo providers currently assigned to this promo view, sorted in order of priority.
+    public var providers: [PromoProvider]?
+
+    // MARK: - View Creation
+
+    public convenience init(frame: CGRect, providers: [PromoProvider]) {
+        self.init(frame: frame)
+        self.providers = providers
+    }
+
     public override init(frame: CGRect) {
+        backgroundView = UIView()
+        if #available(iOS 13.0, *) {
+            backgroundView.backgroundColor = .secondarySystemBackground
+        } else {
+            backgroundView.backgroundColor = .init(white: 0.2, alpha: 1.0)
+        }
         super.init(frame: frame)
-        backgroundColor = .red
+        addSubview(backgroundView)
     }
-
-    public required init?(coder: NSCoder) {
-        super.init(coder: coder)
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: - View Layout
 
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        backgroundView.frame = bounds
+    }
 }
