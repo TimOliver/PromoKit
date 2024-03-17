bc9ce160872d02d551e77177e89fdc0970a64350
Tim Oliver <git@tim.dev>
2024-03-17 17:05:04 +0900

    Deleted identifier from content view (may bring back later)

--- PromoKit/PromoContentView.swift
+++ PromoKit/PromoContentView.swift
@@ -13,14 +13,6 @@ import UIKit
 /// different providers to use the same content view.
 @objc(PMKContentView)
 public protocol PromoContentView: AnyObject {
-
-    /// A reuse identifier assigned to this instance of a content view so
-    /// that it may get recycled by a hosting promo view for different providers.
-    @objc var reuseIdentifier: String { get }
-
-    /// Creates a new instance of a content view with the provided reuse identifier
-    init(reuseIdentifier: String)
-
     /// Called after a content view instance has been reclaimed in order to get it ready for its next use.
     func prepareForReuse()
 }
