eb76ad4ae8239aed8aa28f6d725a34eeb975cdfc
Tim Oliver <git@tim.dev>
2024-03-17 17:04:41 +0900

    Added content fetch definitions to promo provider

--- PromoKit/PromoProvider.swift
+++ PromoKit/PromoProvider.swift
@@ -7,6 +7,15 @@
 
 import UIKit
 
+/// When querying for new content, these are the types of results that may be returned
+@objc(PMKPromoProviderFetchContentResult)
+enum PromoProviderFetchContentResult: Int {
+    case offlineAvailable    = 0 /// An offline version is available, whether default content, or valid cache from a previous request.
+    case noNewContent        = 1 /// The query succeeded, but there was no new content to show.
+    case newContentAvailable = 2 /// The query succeeded, and there is new content to show.
+    case fetchRequestFailed  = 3 /// An error occurred (eg, no internet) and another attempt should be made.
+};
+
 /// A promo provider is a model object that manages fetching data for a promo item
 /// and configuring a promo content view with that data.
 @objc(PMKPromoProvider)
@@ -19,6 +28,14 @@ public protocol PromoProvider: AnyObject {
     /// Default is `nil`, which defaults back to the background color state of the promo view.
     @objc var backgroundColor: UIColor? { get }
 
+    /// Indicates that this provider requires an active internet connection (Default is false).
+    /// If this is set to true, and the device doesn't have an internet connection, this provider
+    /// will be deferred and then tried again once a valid connection is detected.
+    @objc var isInternetAccessRequired: Bool { get }
+
+    /// The type of content view that will be used to present the data managed by this provider.
+    @objc var contentViewClass: AnyClass { get }
+
     /// The amount that the content view is inset by, from the boundary of the promo view.
     /// A different value can be provided depending on the current size class of the promo view.
     /// - Parameter promoView: The promo view hosting the content managed by this provider
@@ -26,18 +43,12 @@ public protocol PromoProvider: AnyObject {
     @objc func contentInsets(for promoView: UIView) -> UIEdgeInsets
 }
 
-// MARK: - Default Implementations
+// MARK: - Default Protocol Implementations
 
 extension PromoProvider {
-
-    // Return the name of the provider object as a default
-    public var identifier: String {
-        String(describing: type(of: self))
-    }
-
-    // Return nil for the default background color
+    public var identifier: String { String(describing: type(of: self)) }
     public var backgroundColor: UIColor? { nil }
-
-    // Return nil for the default amount of insetting
+    public var isInternetAccessRequired: Bool { false }
+    public var contentViewClass: AnyClass { UIView.self }
     public func contentInsets(for promoView: UIView) -> UIEdgeInsets { .zero }
 }
