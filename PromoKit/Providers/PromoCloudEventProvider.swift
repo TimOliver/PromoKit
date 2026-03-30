0be4a3581347f586b005018c9101bf298b220bdd
Tim Oliver <git@tim.dev>
2026-03-30 14:37:37 +0900

    Added missing documentation

--- PromoKit/Providers/PromoCloudEventProvider.swift
+++ PromoKit/Providers/PromoCloudEventProvider.swift
@@ -99,6 +99,7 @@ public class PromoCloudEventProvider: NSObject, PromoProvider {
 
     /// Create a new instance of this provider with the specified CloudKit container name
     /// - Parameter containerIdentifier: The container name to use (eg iCloud.dev.tim.promokit). Specify nil for the app's default container
+    /// - Parameter eventType: An optional type to filter for (eg, app-specific announcements vs global announcements)
     public init(recordType: String = "PromoEvent", containerIdentifier: String? = nil, eventType: String? = nil) {
         self.recordType = recordType
         self.containerIdentifier = containerIdentifier
